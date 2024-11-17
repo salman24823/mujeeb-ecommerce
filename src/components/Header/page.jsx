8b47cc531a7994adf2ac64dcf8696d2ef1227f0c
salman24823 <salman248shah@gmail.com>
2024-11-17 18:28:38 +0500

    header fix

--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -1,15 +1,55 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import { UserRound } from "lucide-react";
-import React from "react";
 
 const Header = () => {
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    // Whenever the route changes, update the state
+    setCurrentPath(window.location.pathname);
+    console.log(currentPath,"current path")
+
+  }, [window.location.pathname]);
+
+
+  // Define the title based on the current URL path
+  const getTitle = () => {
+    switch (currentPath) {
+      case "/panel":
+        return "News";
+      case "/panel/cart":
+        return "Cart";
+      case "/panel/change-password":
+        return "Change Password";
+      case "/panel/dumps-no-pin":
+        return "Dumps without Pin";
+      case "/panel/dumps-with-pin":
+        return "Dumps with Pin";
+      case "/panel/purchases":
+        return "My Purchase History";
+      case "/panel/support":
+        return "Support";
+      case "/panel/wallet":
+        return "My Wallet";
+      default:
+        return "Error"; // Default title for unmatched paths
+    }
+  };
+
   return (
-    <div className="bg-gray-900 border-b border-gray-700 h-14">
+    <div className="bg-gray-900 border-b flex border-gray-700 h-14">
       <div className="w-64 px-2 text-gray-500 gap-2 flex items-center h-full">
-        <UserRound />
+        <UserRound size={24} className="text-gray-500" />
         <p className="text-gray-500">Lorem Ipsum</p>
+
       </div>
 
-      <div>ABC</div>
+      <div className="flex-1 flex items-center justify-center text-xl font-semibold">
+        <h1 className="text-gray-200">{getTitle()}</h1>
+        {/* <h1 className="text-gray-200"> {currentPath} </h1> */}
+      </div>
     </div>
   );
 };
