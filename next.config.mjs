0ae86dca0ba86df55d053d9cc431860acbfa1255
salman24823 <salman248shah@gmail.com>
2024-11-24 10:12:08 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
 
-module.exports = {
     env: {
         NEXTAUTH_URL: process.env.NODE_ENV === 'production'
             ? 'https://nl-9834website.vercel.app'
             : 'http://localhost:3000',
     },
+
 };
 
 
-export default nextConfig;
